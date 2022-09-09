afcc678d97b8dd912384bd7494b264454462a854
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-08 18:39:51 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+* main - print something
+* Return: nothing
+*/
+#include <stdio.h>
+int main(void)
+{
+	char hello[] = "\"Programming is like building a multilingual puzzle\n";
+	puts(hello + "\n");
+	return (0);
+}
