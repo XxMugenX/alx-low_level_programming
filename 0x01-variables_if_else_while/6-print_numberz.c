019b382d2d37c250f905491db0721e8aec3c658b
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-09 21:59:45 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - prints numbers
+ *
+ * Return: nothing
+ */
+
+int main(void)
+
+{
+
+int i;
+
+for (i = '0' ; i <= '9' ; i++)
+
+{
+
+putchar(i);
+
+}
+
+putchar('\n');
+
+return (0);
+}
