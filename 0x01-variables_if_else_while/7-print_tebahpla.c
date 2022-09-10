1884e93f927533d5abe43acfc7538c89b88300c2
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-09 22:07:16 -0700

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+/**
+ *main - reverse alphabet
+ *
+ *Return: nothing
+ */
+#include <stdio.h>
+int main(void)
+{
+
+int x;
+
+for  (x = 'z' ; x >= 'a' ; x--)
+
+{
+
+putchar(x);
+
+}
+
+putchar('\n');
+
+return (0);
+}
