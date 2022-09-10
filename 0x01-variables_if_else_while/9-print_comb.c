b061be3a8dcdf08157b5ad9f982e763a5e661890
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-09 22:51:40 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - comma
+ *
+ *Return: nothing
+ */
+int main(void)
+{
+int i;
+
+i = '0';
+
+while (i < '9')
+{
+for (i ; i <= '8' ; i++)
+
+{
+putchar(i);
+
+putchar(',');
+
+putchar(' ');
+}
+}
+putchar('9');
+
+return (0);
+}
