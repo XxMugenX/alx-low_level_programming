e50262035dab4e84d6c8958630c6de800005d721
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-14 15:01:01 -0700

    nest

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_isalpha - returns true for an alphabet
+ *
+ *@c: character
+ *Return: zero
+ */
+int _isalpha(int c)
+{
+if (isalpha(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
