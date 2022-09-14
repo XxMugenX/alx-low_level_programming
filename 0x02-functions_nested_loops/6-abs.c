3ec08155cc4f32e6cd0a163eda77b03e5994b630
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-14 15:29:10 -0700

    nest

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_abs - prints absolute value of a number
+ *
+ *@int: integer
+ *
+ *Return: zero
+ */
+int _abs(int)
+{
+
+abs(int);
+
+return (0);
+}
