4319bf2722ae5a858db00bf4ea1ea7b24ba5a15e
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-14 04:27:23 -0700

    nest

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+void print_alphabet(void);
+/**
+ * print_alphabet - prints alphabet
+ *
+ */
+
+void print_alphabet(void)
+{
+int x;
+
+for (x = 'a'; x <= 'z'; x++)
+	putchar(x);
+
+putchar('\n');
+return;
+}
