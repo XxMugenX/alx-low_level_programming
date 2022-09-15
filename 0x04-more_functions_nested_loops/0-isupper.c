e0df0e457f31e70dc446008c740eebe716a27584
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-15 15:49:26 -0700

    nest

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_isupper - returns 1 or 0 if tur or false
+ *@c: char
+ *Return:1 and 0
+ */
+
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
