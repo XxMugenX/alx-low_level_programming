b353132eed773ab27c546fc1f91d19f566ee7c37
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-16 16:18:03 -0700

    nest

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *main - fizzbuzz
+ *Return: nothing
+ */
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz");
+putchar(' ');
+}
+else if ((i % 3) == 0)
+{
+printf("Fizz");
+putchar(' ');
+}
+else if ((i % 5) == 0)
+{
+printf("Buzz");
+putchar(' ');
+}
+else
+{
+printf("%d", i);
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
