75dc392a83a765024310ed1def72cd36b098ce94
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-16 14:59:25 -0700

    nest

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_square - prints a square
+ *@size: integer
+ *Return: nothing
+ */
+void print_square(int size)
+{
+int i;
+
+int j;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
+return;
+}
