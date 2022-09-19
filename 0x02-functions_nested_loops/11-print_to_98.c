35232d1e37dda3f906fe9b1aab27c20e9943026f
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-19 03:45:53 -0700

    nest

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,17 @@ i = n;
 
 if (i > 98)
 {
-for (i; i >= 99; i--)
-{
+for (i = n; i >= 99; i--)
+
 printf("%d, ", i);
-}
+
 }
 if (i < 98)
 {
-for (i; i <= 97; i++)
-{
+for (i = n; i <= 97; i++)
+
 printf("%d, ", i);
-}
+
 }
 printf("%d", i);
 }
