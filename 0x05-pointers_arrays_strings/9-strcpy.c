6596fd03e4a11fd8af734980af1e1baa5e825642
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-20 16:11:10 -0700

    p

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcpy - copies string of second parameter to the first one
+ *@dest: char
+ *@src: char
+ *Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+strcpy(dest, src);
+
+return;
+}
