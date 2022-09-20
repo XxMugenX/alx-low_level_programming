e709104bc4782342e4aa8d859a608293ee57bda2
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-19 17:21:58 -0700

    points

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,22 @@
 #include "main.h"
+#include <string.h>
 /**
+ *print_rev - prints words in reverse
  *
+ *@s: char
  *
- *
+ *Return: void
  */
 void print_rev(char *s)
 {
+int len;
+
+len = strlen(s);
+
+for (len; len > 0; len--)
+
+_putchar(s[len]);
+
+return;
 
 }
