e489a72691243a72d10e0119b3fee1fdf5025815
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-22 18:51:29 -0700

    p

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -19,18 +19,14 @@ while (str[j] != '\0')
 
 j++;
 
-}
 len = j;
+}
 
 for (i = 0; i < j; i++)
 {
 if (str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] = str[i] - 'a' + 'A';
-}
-else
-{
-return (str);
 }
 }
 return (str);
