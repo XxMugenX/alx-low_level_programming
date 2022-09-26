2ce4e75b36446d2972d29468cce3a0c9424f92d4
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-26 15:40:11 -0700

    m

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strchr - returns a string from the first occurrance of a character
+ *@c: char
+ *@s: char
+ *Return: returns a string fron the first identified letter
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (*s == c)
+{
+return (s);
+}
+return (NULL);
+}
