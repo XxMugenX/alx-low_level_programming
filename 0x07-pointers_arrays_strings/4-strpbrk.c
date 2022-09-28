08a98f7e5bb8ec19dede4bf0a4fc9fc4e9055ec7
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-27 17:51:07 -0700

    m

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_strpbrk - start
+ *@accept: char
+ *@s: char
+ *Return: string break from match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+int i;
+
+int j;
+
+i = 0;
+
+while (*s)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (accept[j] == s[i])
+{
+return (s);
+}
+}
+*s++;
+}
+return (NULL);
+}
