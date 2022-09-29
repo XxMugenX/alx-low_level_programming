1739eeba86977f579a686c496613a0bb9112ee0c
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-28 21:02:19 -0700

    r

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_pow_recursion - entery
+ *@y: integer
+ *@x: integer
+ *Return: x raised to power y ,if y is null it returns -1
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < -1)
+{
+return (-1);
+}
+if (y != 0)
+{
+y--;
+return (x * _pow_recursion(x, y));
+}
+}
