eea065dbe0184eeb50d7ef346725630d443d571c
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-28 19:58:19 -0700

    r

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - start
+ *@s: char
+ *Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+if (*str)
+{
+_print_rev_recursion(s + 1);
+
+_putchar(*s);
+
+return;
+}
+
+}
