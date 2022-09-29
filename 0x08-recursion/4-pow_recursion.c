1d4446921adbdc8b2de933e1af068b1ed2d66dc2
Peace Akhaze <peaceakhaze017@gmail.com>
2022-09-28 21:10:04 -0700

    r

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  *_pow_recursion - entery
  *@y: integer
  *@x: integer
- *Return: x raised to power y ,if y is null it returns -1
+ *Return: x raised to power y ,if y is null it returns error
  */
 int _pow_recursion(int x, int y)
 {
@@ -15,5 +15,4 @@ if (y != 0)
 {
 y--;
 return (x * _pow_recursion(x, y));
-}
-}
+}}
