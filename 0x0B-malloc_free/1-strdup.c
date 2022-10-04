3d74e9ba182c59f03adb7410df95a8f6ddff2dbb
Peace Akhaze <peaceakhaze017@gmail.com>
2022-10-04 16:32:01 -0700

    mem allocation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup - duplicates string to another memory
+ *@str: char pointer
+ *Return: returns duplicate string
+ */
+char *_strdup(char *str)
+{
+char *dup;
+
+if (str == NULL)
+{
+return (NULL);
+}
+else
+{
+
+dup = malloc(sizeof(char));
+
+dup = str;
+
+return (dup);
+}
+return (dup);
+}
