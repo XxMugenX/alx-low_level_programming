b93e35f5ffa7f4aaaf53ee6dcd0705f99e051989
Peace Akhaze <peaceakhaze017@gmail.com>
2022-10-05 17:22:54 -0700

    arg

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ *main - entry
+ *@argc: integer
+ *@argv: char
+ *Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int sum;
+
+sum = 0;
+
+for (i = 1; i < argc; i++)
+{
+if (isdigit(*argv[i]))
+{
+sum += atoi(argv[i]);
+}
+else
+{
+printf("Error\n");
+
+return (1);
+}
+}
+printf("%d\n", sum);
+
+return (0);
+}
