09b7f510368e6f7e001d28aa9c7cb70556fa1728
Peace Akhaze <peaceakhaze017@gmail.com>
2022-10-12 18:26:53 -0700

    ftp

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+/**
+ *int_index - iterates through array and feeds value of array to cmp function
+ *@cmp: function for finding match
+ *@size: size of array in int
+ *@array: int one dimensional array
+ *Return: returns array index of match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+int i;
+
+int sto;
+
+if (size <= 0)
+{
+return (-1);
+}
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+sto = i;
+
+i = size + 1;
+
+return (sto);
+}
+}
+return (-1);
+}
