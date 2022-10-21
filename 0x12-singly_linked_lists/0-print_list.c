3a4c367a47a62d0ebe34397f43c5c81f4d2e42d9
Peace Akhaze <peaceakhaze017@gmail.com>
2022-10-20 20:43:09 -0700

    links

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "list.h"
+/**
+ *print_list - prints elements in a linked list
+ *@h: the list
+ *
+ *Return: gives the number of nodes in h
+ */
+size_t print_list(const list_t *h)
+{
+size_t nodes = 0;
+
+while (h)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+
+else
+
+printf("[%d] %s\n", h->len, h->str);
+
+node++;
+
+h = h->next;
+
+}
+return (nodes);
+}
---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,10 @@
+#ifndef LIST_H
+
+#define LIST_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
